84230ad77bb09d51614e6c0e1aa75819e3ebd9f4
elirex <sheng79225@gmail.com>
2021-06-08 01:01:44 +0800

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.0"
+}
+
+group = "com.elirex"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
