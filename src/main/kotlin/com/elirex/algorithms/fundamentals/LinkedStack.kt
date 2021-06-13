d1a697f4943ac75fe4c6e6099c1fd5d817015869
elirex <sheng79225@gmail.com>
2021-06-13 16:35:56 +0800

    [fundamentals] Implements LinkedStack

---
+++ src/main/kotlin/com/elirex/algorithms/fundamentals/LinkedStack.kt
@@ -0,0 +1,65 @@
+package com.elirex.algorithms.fundamentals
+
+/**
+ * Created by Wang, Sheng-Yuan (Elirex) on 2021/6/13.
+ */
+class LinkedStack<Item> : Stack<Item> {
+
+    private var first: Node? = null
+    private var size: Int = 0
+
+    override fun push(item: Item) {
+        first = Node(item, first)
+        size++
+    }
+
+    override fun pop(): Item? {
+        if (isEmpty()) return null
+        val item = first?.item
+        first = first?.next
+        size--
+        return item
+    }
+
+    override fun peek(): Item? {
+        return first?.item
+    }
+
+    override fun isEmpty(): Boolean = first == null
+
+    override fun size(): Int = size
+
+    override fun iterator(): Iterator<Item> = LinkedIterator()
+
+    private inner class LinkedIterator : Iterator<Item> {
+        private var current: Node? = first
+        override fun hasNext(): Boolean = current != null
+
+        override fun next(): Item {
+            if (hasNext().not()) throw NoSuchElementException()
+            val item: Item = current?.item ?: throw NoSuchElementException()
+            current = current?.next
+            return item
+        }
+
+    }
+
+    private inner class Node(val item: Item, var next: Node? = null)
+}
+
+fun main(args: Array<String>) {
+    val stack: Stack<String> = LinkedStack<String>()
+    stack.push("A")
+    stack.push("B")
+    stack.push("C")
+    stack.push("D")
+    stack.push("E")
+    stack.push("F")
+
+    for (element in stack) {
+        println(element)
+    }
+
+    println("pop: ${stack.pop()}")
+    println("peek: ${stack.peek()}")
+}
