13a5ba2e6d8868861fd1c161b8d3536379ac62c8
elirex <sheng79225@gmail.com>
2022-11-15 01:33:44 +0800

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.5.0"
+    kotlin("jvm") version "1.7.20"
+    application
 }
 
 group = "com.elirex"
-version = "1.0.0"
+version = "1.0-0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
 }
